38b3dde1483e3ddd4a668971e56ba77baa71e0d7
ShanelleDejito <yagonocyrus@gmail.com>
2025-08-27 09:46:29 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAO85--DlqK2i2fHd_e7-5Se_c7cvznzjQ",
+  authDomain: "cj-goals.firebaseapp.com",
+  projectId: "cj-goals",
+  storageBucket: "cj-goals.firebasestorage.app",
+  messagingSenderId: "108188556303",
+  appId: "1:108188556303:web:4a696042c889d7a62cd77a"
+};
+
+
+const app = initializeApp(firebaseConfig);
